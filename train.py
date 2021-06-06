81d8493c416fdd3454e07cb108a0fc3a6665168e
lucaslingle <lucasdaxlingle@gmail.com>
2021-06-06 15:20:36 -0700

    commit updates

--- train.py
+++ train.py
@@ -307,8 +307,8 @@ def training_loop(
         ppo_opt_epochs: optimization epochs for proximal policy optimization.
         ppo_clip_param: clip parameter for proximal policy optimization.
         ppo_ent_coef: entropy bonus coefficient for proximal policy optimization
-        max_pol_iters: maximum policy improvements to make.
-        pol_iters_so_far: policy improvements so far.
+        max_pol_iters: the maximum number policy improvements to make.
+        pol_iters_so_far: the number of policy improvements made so far.
         policy_checkpoint_fn: a callback for saving checkpoints of policy net.
         value_checkpoint_fn: a callback for saving checkpoints of value net.
 
