10fa0e147565d81e6fbfeb044ac4f54d129dcbcb
lucaslingle <lucasdaxlingle@gmail.com>
2021-07-07 02:30:23 -0700

    commit updated defaults

--- train_mdp.py
+++ train_mdp.py
@@ -24,7 +24,7 @@ def create_argparser():
     parser.add_argument("--num_states", type=int, default=10)
     parser.add_argument("--num_actions", type=int, default=5)
     parser.add_argument("--num_features", type=int, default=256)
-    parser.add_argument("--use_wn", type=int, choices=[0,1], default=1)
+    parser.add_argument("--use_wn", type=int, choices=[0,1], default=0)
     parser.add_argument("--model_name", type=str, default='defaults')
     parser.add_argument("--checkpoint_dir", type=str, default='checkpoints')
     parser.add_argument("--checkpoint_interval", type=int, default=10)
@@ -32,13 +32,13 @@ def create_argparser():
     parser.add_argument("--episodes_per_meta_episode", type=int, default=10)
     parser.add_argument("--meta_episodes_per_policy_update", type=int, default=30000//100)
     parser.add_argument("--meta_episodes_per_actor_batch", type=int, default=60)
-    parser.add_argument("--ppo_opt_epochs", type=int, default=40)
+    parser.add_argument("--ppo_opt_epochs", type=int, default=8)
     parser.add_argument("--ppo_clip_param", type=float, default=0.10)
     parser.add_argument("--ppo_ent_coef", type=float, default=0.01)
     parser.add_argument("--discount_gamma", type=float, default=0.99)
     parser.add_argument("--gae_lambda", type=float, default=0.3)
-    parser.add_argument("--adam_lr", type=float, default=1e-4)
-    parser.add_argument("--adam_eps", type=float, default=1e-3)
+    parser.add_argument("--adam_lr", type=float, default=2e-4)
+    parser.add_argument("--adam_eps", type=float, default=1e-5)
     parser.add_argument("--experiment_seed", type=int, default=0) # not yet used
     return parser
 
