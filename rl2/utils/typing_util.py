7f7d116966a1d09f5ac5e8dcbaab87141f5d34fe
lucaslingle <lucasdaxlingle@gmail.com>
2021-12-29 22:32:53 -0800

    commit updates

---
+++ rl2/utils/typing_util.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+import torch as tc
+
+from rl2.agents.integration.policy_net import StatefulPolicyNet
+from rl2.agents.integration.value_net import StatefulValueNet
+from rl2.envs.abstract import MetaEpisodicEnv
+from rl2.algos.common import MetaEpisode
+
+
+MetaEpisodicEnv = MetaEpisodicEnv
+MetaEpisode = MetaEpisode
+
+Module = tc.nn.Module
+Optimizer = tc.optim.Optimizer
+Scheduler = tc.optim.lr_scheduler._LRScheduler
+Checkpointable = Union[Module, Optimizer, Scheduler]
+
+DDP = tc.nn.parallel.DistributedDataParallel
+StatefulPolicyNet = Union[DDP, StatefulPolicyNet]
+StatefulValueNet = Union[DDP, StatefulValueNet]
--- train.py
+++ train.py
@@ -3,7 +3,7 @@ Script for training stateful meta-reinforcement learning agents
 """
 
 import argparse
-from functools import partial
+import os
 
 import torch as tc
 
@@ -19,11 +19,10 @@ from rl2.agents.heads.policy_heads import LinearPolicyHead
 from rl2.agents.heads.value_heads import LinearValueHead
 from rl2.agents.integration.policy_net import StatefulPolicyNet
 from rl2.agents.integration.value_net import StatefulValueNet
+from rl2.agents.integration.ddp import StatefulDDP
 from rl2.algos.ppo import training_loop
 
-from rl2.utils.checkpoint_util import maybe_load_checkpoint, save_checkpoint
-from rl2.utils.comm_util import get_comm, sync_state
-from rl2.utils.constants import ROOT_RANK
+from rl2.utils.checkpoint_util import maybe_load_checkpoints
 from rl2.utils.optim_util import get_weight_decay_param_groups
 
 
@@ -31,8 +30,13 @@ def create_argparser():
     parser = argparse.ArgumentParser(
         description="""Training script for RL^2.""")
 
+    ### Distributed Training and Persistence
+    parser.add_argument("--world_size", type=int, default=8)
+    parser.add_argument("--models_dir", type=str, default='models_dir')
+    parser.add_argument("--run_name", type=str, default='defaults')
+
     ### Environment
-    parser.add_argument("--environment", choices=['bandit', 'tabular_mdp'],
+    parser.add_argument("--environment_name", choices=['bandit', 'tabular_mdp'],
                         default='bandit')
     parser.add_argument("--num_states", type=int, default=10,
                         help="Ignored if environment is bandit.")
@@ -49,10 +53,6 @@ def create_argparser():
         default='gru')
     parser.add_argument("--num_features", type=int, default=256)
 
-    ### Checkpointing
-    parser.add_argument("--model_name", type=str, default='defaults')
-    parser.add_argument("--checkpoint_dir", type=str, default='checkpoints')
-
     ### Training
     parser.add_argument("--max_pol_iters", type=int, default=12000)
     parser.add_argument("--meta_episodes_per_policy_update", type=int, default=-1,
@@ -63,18 +63,17 @@ def create_argparser():
     parser.add_argument("--ppo_ent_coef", type=float, default=0.01)
     parser.add_argument("--discount_gamma", type=float, default=0.99)
     parser.add_argument("--gae_lambda", type=float, default=0.3)
-    parser.add_argument("--standardize_advs", type=int, choices=[0,1], default=0)
     parser.add_argument("--adam_lr", type=float, default=2e-4)
     parser.add_argument("--adam_eps", type=float, default=1e-5)
     parser.add_argument("--adam_wd", type=float, default=0.01)
     return parser
 
 
-def create_env(environment, num_states, num_actions, max_episode_len):
-    if environment == 'bandit':
+def create_env(environment_name, num_states, num_actions, max_episode_len):
+    if environment_name == 'bandit':
         return BanditEnv(
             num_actions=num_actions)
-    if environment == 'tabular_mdp':
+    if environment_name == 'tabular_mdp':
         return MDPEnv(
             num_states=num_states,
             num_actions=num_actions,
@@ -82,18 +81,18 @@ def create_env(environment, num_states, num_actions, max_episode_len):
     raise NotImplementedError
 
 
-def create_preprocessing(environment, num_states, num_actions):
-    if environment == 'bandit':
+def create_preprocessing(environment_name, num_states, num_actions):
+    if environment_name == 'bandit':
         return MABPreprocessing(
             num_actions=num_actions)
-    if environment == 'tabular_mdp':
+    if environment_name == 'tabular_mdp':
         return MDPPreprocessing(
             num_states=num_states,
             num_actions=num_actions)
     raise NotImplementedError
 
 
-def create_architecture(architecture, input_dim, num_features, context_size):
+def create_architecture(architecture, input_dim, num_features, meta_episode_len):
     if architecture == 'gru':
         return GRU(
             input_dim=input_dim,
@@ -111,7 +110,7 @@ def create_architecture(architecture, input_dim, num_features, context_size):
         return SNAIL(
             input_dim=input_dim,
             feature_dim=num_features,
-            context_size=context_size,
+            context_size=meta_episode_len,
             use_ln=True)
     if architecture == 'transformer':
         return Transformer(
@@ -119,7 +118,7 @@ def create_architecture(architecture, input_dim, num_features, context_size):
             feature_dim=num_features,
             n_layer=9,
             n_head=2,
-            n_context=context_size)
+            n_context=meta_episode_len)
     raise NotImplementedError
 
 
@@ -135,65 +134,44 @@ def create_head(head_type, num_features, num_actions):
 
 
 def create_net(
-        net_type, environment, architecture, num_states, num_actions,
-        num_features, context_size
+        net_type, environment_name, architecture, num_states, num_actions,
+        num_features, meta_episode_len, **kwargs
 ):
+    assert net_type in ['policy', 'value']
     preprocessing = create_preprocessing(
-        environment=environment,
+        environment_name=environment_name,
         num_states=num_states,
         num_actions=num_actions)
     architecture = create_architecture(
         architecture=architecture,
         input_dim=preprocessing.output_dim,
         num_features=num_features,
-        context_size=context_size)
+        meta_episode_len=meta_episode_len)
     head = create_head(
         head_type=net_type,
         num_features=architecture.output_dim,
         num_actions=num_actions)
 
-    if net_type == 'policy':
-        return StatefulPolicyNet(
-            preprocessing=preprocessing,
-            architecture=architecture,
-            policy_head=head)
-    if net_type == 'value':
-        return StatefulValueNet(
-            preprocessing=preprocessing,
-            architecture=architecture,
-            value_head=head)
-    raise NotImplementedError
+    cls = {'policy': StatefulPolicyNet, 'value': StatefulValueNet}[net_type]
+    return StatefulDDP(cls(preprocessing, architecture, head))
 
 
-def main():
-    args = create_argparser().parse_args()
-    comm = get_comm()
+def setup(rank, args):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12345'
+    tc.distributed.init_process_group(
+        backend='gloo', rank=rank, world_size=args.world_size)
 
     # create env.
     env = create_env(
-        environment=args.environment,
+        environment_name=args.environment_name,
         num_states=args.num_states,
         num_actions=args.num_actions,
         max_episode_len=args.max_episode_len)
 
     # create learning system.
-    policy_net = create_net(
-        net_type='policy',
-        environment=args.environment,
-        architecture=args.architecture,
-        num_states=args.num_states,
-        num_actions=args.num_actions,
-        num_features=args.num_features,
-        context_size=args.meta_episode_len)
-
-    value_net = create_net(
-        net_type='value',
-        environment=args.environment,
-        architecture=args.architecture,
-        num_states=args.num_states,
-        num_actions=args.num_actions,
-        num_features=args.num_features,
-        context_size=args.meta_episode_len)
+    policy_net = create_net('policy', **vars(args))
+    value_net = create_net('value', **vars(args))
 
     policy_optimizer = tc.optim.AdamW(
         get_weight_decay_param_groups(policy_net, args.adam_wd),
@@ -208,92 +186,59 @@ def main():
     value_scheduler = None
 
     # load checkpoint, if applicable.
-    pol_iters_so_far = 0
-    if comm.Get_rank() == ROOT_RANK:
-        a = maybe_load_checkpoint(
-            checkpoint_dir=args.checkpoint_dir,
-            model_name=f"{args.model_name}/policy_net",
-            model=policy_net,
-            optimizer=policy_optimizer,
-            scheduler=policy_scheduler,
-            steps=None)
-
-        b = maybe_load_checkpoint(
-            checkpoint_dir=args.checkpoint_dir,
-            model_name=f"{args.model_name}/value_net",
-            model=value_net,
-            optimizer=value_optimizer,
-            scheduler=value_scheduler,
-            steps=None)
-
-        if a != b:
-            raise RuntimeError(
-                "Policy and value iterates not aligned in latest checkpoint!")
-        pol_iters_so_far = a
-
-    # sync state.
-    pol_iters_so_far = comm.bcast(pol_iters_so_far, root=ROOT_RANK)
-    sync_state(
-        model=policy_net,
-        optimizer=policy_optimizer,
-        scheduler=policy_scheduler,
-        comm=comm,
-        root=ROOT_RANK)
-    sync_state(
-        model=value_net,
-        optimizer=value_optimizer,
-        scheduler=value_scheduler,
-        comm=comm,
-        root=ROOT_RANK)
-
-    # make callback functions for checkpointing.
-    policy_checkpoint_fn = partial(
-        save_checkpoint,
+    args.checkpoint_dir = os.path.join(args.models_dir, f"{args.run_name}")
+    a = maybe_load_checkpoints(
         checkpoint_dir=args.checkpoint_dir,
-        model_name=f"{args.model_name}/policy_net",
-        model=policy_net,
-        optimizer=policy_optimizer,
-        scheduler=policy_scheduler)
-
-    value_checkpoint_fn = partial(
-        save_checkpoint,
+        checkpointables={
+            'policy_net': policy_net,
+            'policy_optimizer': policy_optimizer,
+            'policy_scheduler': policy_scheduler
+        },
+        map_location='cpu',
+        steps=None)
+    b = maybe_load_checkpoints(
         checkpoint_dir=args.checkpoint_dir,
-        model_name=f"{args.model_name}/value_net",
-        model=value_net,
-        optimizer=value_optimizer,
-        scheduler=value_scheduler)
+        checkpointables={
+            'value_net': value_net,
+            'value_optimizer': value_optimizer,
+            'value_scheduler': value_scheduler
+        },
+        map_location='cpu',
+        steps=None)
+    if a != b:
+        msg = 'Policy and value iterates not aligned in latest checkpoint!'
+        raise RuntimeError(msg)
 
-    # run it!
+    return {
+        'env': env,
+        'policy_net': policy_net,
+        'policy_optimizer': policy_optimizer,
+        'policy_scheduler': policy_scheduler,
+        'value_net': value_net,
+        'value_optimizer': value_optimizer,
+        'value_scheduler': value_scheduler,
+        'pol_iters_so_far': a
+    }
+
+
+def cleanup():
+    tc.distributed.destroy_process_group()
+
+
+def train(rank, args):
+    learning_system = setup(rank, args)
+
     if args.meta_episodes_per_policy_update == -1:
         numer = 240000
-        denom = comm.Get_size() * args.meta_episode_len
-        meta_episodes_per_policy_update = numer // denom
-    else:
-        meta_episodes_per_policy_update = args.meta_episodes_per_policy_update
+        denom = args.world_size * args.meta_episode_len
+        args.meta_episodes_per_policy_update = numer // denom
 
-    training_loop(
-        env=env,
-        policy_net=policy_net,
-        value_net=value_net,
-        policy_optimizer=policy_optimizer,
-        value_optimizer=value_optimizer,
-        policy_scheduler=policy_scheduler,
-        value_scheduler=value_scheduler,
-        meta_episodes_per_policy_update=meta_episodes_per_policy_update,
-        meta_episodes_per_learner_batch=args.meta_episodes_per_learner_batch,
-        meta_episode_len=args.meta_episode_len,
-        ppo_opt_epochs=args.ppo_opt_epochs,
-        ppo_clip_param=args.ppo_clip_param,
-        ppo_ent_coef=args.ppo_ent_coef,
-        discount_gamma=args.discount_gamma,
-        gae_lambda=args.gae_lambda,
-        standardize_advs=bool(args.standardize_advs),
-        max_pol_iters=args.max_pol_iters,
-        pol_iters_so_far=pol_iters_so_far,
-        policy_checkpoint_fn=policy_checkpoint_fn,
-        value_checkpoint_fn=value_checkpoint_fn,
-        comm=comm)
+    training_loop(rank, **learning_system, **vars(args))
+    cleanup()
 
 
 if __name__ == '__main__':
-    main()
+    args = create_argparser().parse_args()
+    tc.multiprocessing.spawn(
+        fn=train, args=(args,), nprocs=args.world_size, join=True
+    )
